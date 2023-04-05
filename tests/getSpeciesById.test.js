8c1723698816d5ac052f7208b6a7afcb43223f3c
Mayk <maycon.lrgo@gmail.com>
2023-04-05 00:28:57 -0300

    feat: finalização req 1

--- tests/getSpeciesById.test.js
+++ tests/getSpeciesById.test.js
@@ -41,5 +41,61 @@ describe("getSpeciesById", () => {
     ];
     expect(getSpecies).toEqual(expected);
   });
-  it("Retorna array com várias espécies se recebe mais de um id", () => {});
+  it("Retorna array com várias espécies se recebe mais de um id", () => {
+    const getSpecies = getSpeciesById(
+      "0938aa23-f153-4937-9f88-4858b24d6bce",
+      "89be95b3-47e4-4c5b-b687-1fabf2afa274"
+    );
+    const expected = [
+      {
+        id: "0938aa23-f153-4937-9f88-4858b24d6bce",
+        name: "lions",
+        popularity: 4,
+        location: "NE",
+        availability: ["Tuesday", "Thursday", "Saturday", "Sunday"],
+        residents: [
+          {
+            name: "Zena",
+            sex: "female",
+            age: 12,
+          },
+          {
+            name: "Maxwell",
+            sex: "male",
+            age: 15,
+          },
+          {
+            name: "Faustino",
+            sex: "male",
+            age: 7,
+          },
+          {
+            name: "Dee",
+            sex: "female",
+            age: 14,
+          },
+        ],
+      },
+      {
+        id: "89be95b3-47e4-4c5b-b687-1fabf2afa274",
+        name: "frogs",
+        popularity: 2,
+        location: "SW",
+        availability: ["Saturday", "Friday", "Thursday", "Wednesday"],
+        residents: [
+          {
+            name: "Cathey",
+            sex: "female",
+            age: 3,
+          },
+          {
+            name: "Annice",
+            sex: "female",
+            age: 2,
+          },
+        ],
+      },
+    ];
+    expect(getSpecies).toEqual(expected);
+  });
 });
